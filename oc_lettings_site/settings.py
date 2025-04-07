fcee4ff20d3c0e67b46508b93065af9957de8dcc
Damien Vincelot <damien.vincelot@gmail.com>
2025-04-07 15:58:18 +0200

    Update

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static",]
+STATICFILES_DIRS = [BASE_DIR / "static", ]
 
 
 # Initialisation de Sentry
