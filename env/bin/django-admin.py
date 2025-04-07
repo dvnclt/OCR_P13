a2f34a4994b280cce4e9533e0a12e73eb11b2e2d
Damien Vincelot <damien.vincelot@gmail.com>
2025-04-07 15:06:14 +0200

    Remove unwanted tracked files now ignored

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Damien/Documents/Dev/Formation Python/projet_13/OCR_P13/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
